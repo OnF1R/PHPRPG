a6e04c85c86f740d09e5bffb457ca5d77031b3a3
Oleg <bender987@bk.ru>
2022-10-19 01:13:47 +0300

    Attack and death function
    basicAttack and death function rework

--- src/Player.php
+++ src/Player.php
@@ -8,31 +8,44 @@ class Player
 
     public $name;
     public $level;
-    public $health;
+    public $maxHealth;
+    public $currentHealth;
     public $damage;
     public $race;
-    
+
     public $luck;
     public $armor;
 
-    public function __construct(int $Health = 10, int $Damage = 2, string $Race, string $Name , $Luck = 0)
+    public $critChance;
+    public $critDamage;
+
+    public function __construct(int $maxHealth = 50, int $Damage = 3, string $Race, string $Name, $Luck = 0)
     {
         $this->level = 1;
-        $this->health = $Health;
+        $this->maxHealth = $maxHealth;
         $this->damage = $Damage;
         $this->race = $Race;
         $this->name = $Name;
         $this->luck = $Luck;
+        $this->armor = 0;
+        $this->critChance = 0;
+        $this->critDamage = 0;
         $this->inventory = new Inventory;
+
+        $this->currentHealth = $this->maxHealth;
     }
 
     public function basicAttack($enemy)
     {
-        $enemy->health -= $this->damage;
-        if ($enemy->health <= 0) {
-            return $enemy->death($this);
-        } else {
-            return $enemy->name . " получил " . $this->damage . " \e[1;31mурона\e[0m, его \e[1;32mздоровье\e[0m " . $enemy->health . "\n";
-        }
+        $dealedDamage = $this->damage;
+
+        $enemy->fightLogic($this, $dealedDamage);
+    }
+
+    public function death($player)
+    {
+        echo "К сожалению " . $this->name . " \e[1;31mумер\e[0m\n";
+        $this->currentHealth = $this->maxHealth;
+        return 0;
     }
 }
